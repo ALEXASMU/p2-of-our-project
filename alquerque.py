c4809ea454d011c960e09f50cca4477d5dce04c0
SAUCYJOEBI <126859372+SAUCYJOEBI@users.noreply.github.com>
2023-12-06 13:48:25 +0100

    Update alquerque.py

--- alquerque.py
+++ alquerque.py
@@ -4,30 +4,13 @@ from minimax import *
 from dataclasses import dataclass
 
 def start_game():
-    """
-    Determines wether or not a player is a bot
-    start_game()
-    Should Player 1 be a bot?: Yes
-    Should Player 2 be a bot?: Yes
-    >>>Player 1 is a bot and so is Player 2
-    """
+
+def introduction():
     print("""
     WELCOME TO ALQUERQUE!!!
-
     """)
-    print("Player 1 is white and Player 2 is black, this is important because white starts")
+    print("Player 1 starts")
+    input("type 1 to play against a human, 2 to play against a bot and 3 to see two bots play against each other: "
+          
 
-    player1 = input("Should Player 1 be a bot? (Yes/No): ")
-    player2 = input("Should Player 2 be a bot? (Yes/No): ")
-#This section can be used to return a bool value that determines human and bot players
-    if player1 == "Yes" and player2 == "No":
-        return('Player 1 is a bot, and Player 2 is a human player')
-    elif player1 == "No" and player2 == "Yes":
-        return('Player 1 is a human player, and Player 2 is a bot')
-    elif player1 == "Yes" and player2 == "Yes":
-        return('Player 1 is a bot and so is Player 2')
-    elif player1 == "No" and player2 == "No":
-        return('both Player 1 & 2 are human players')
-    else:
-        return('Pick either yes or no')
-   
+  
