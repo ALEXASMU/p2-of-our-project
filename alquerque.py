1a6de48fc1ba1ca570e3a6746b7b4d2a0c0bbb95
SAUCYJOEBI <126859372+SAUCYJOEBI@users.noreply.github.com>
2023-12-07 15:35:37 +0100

    Update alquerque.py

--- alquerque.py
+++ alquerque.py
@@ -5,13 +5,34 @@ from dataclasses import dataclass
 
 def start_game():
     """
-
+    initializes the game by telling the player to pick 1 out of 4 options
+    >>> start_game()
+    WELCOME TO ALQUERQUE!!!
+    
+    Player 1 starts
+    type 1 to play against a human
+    type 2 to play against a bot as white
+    type 3 to play against a bot as black
+    type 4 to see two bots play
+    : 4
+    You chose to watch a match between 2 bots
     """
     print("""
     WELCOME TO ALQUERQUE!!!
     """)
-    print("Player 1 starts")
+    print("Player 1 starts, please pick one of the 4 options")
     print("type 1 to play against a human")
-    print("type 2 to play against a bot")
-    print("type 3 to see two bots play")
-    input(": ")
+    print("type 2 to play against a bot as white")
+    print("type 3 to play against a bot as black")
+    print("type 4 to see two bots play")
+    global game_type
+    game_type = int(input(": "))
+    
+    if game_type == 1:
+        print("You chose to play against a human")
+    elif game_type == 2:
+        print("You chose to play against a bot as white")
+    elif game_type == 3:
+        print("You chose to play against a bot as black")
+    elif game_type == 4: 
+        print("You chose to watch a match between 2 bots")
