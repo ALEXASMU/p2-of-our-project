ff3585dc02812981d71f3dcd6c4cf244be009131
Tom T Uni <totho14@student.sdu.dk>
2023-12-13 19:34:45 +0100

    First Sketch
    Game State
    Player Turn
    Bot Play
    Human Player
    (Coordinates translations)

--- alquerque.py
+++ alquerque.py
@@ -40,6 +40,89 @@ def start_menu():
             case _:
                 print("Please select any number 1-4")
 
+def game_state(b: Board) -> None:
+    """Checks wether game over,
+       calls player_turn or game_over.
+    """
+    if is_game_over
+        game_over
+    else:
+        player_turn(b)
+            
+def player_turn(b: Board) -> None:
+    """Either plays for White or Black,
+       using distinct process flow for human/ vs bot play.
+    """
+    if white_plays:
+        print('White is playing.')
+        if game_type == 3 or game_type = 4:
+            bot_play(b)
+        else:
+            human_player(b)
+    else:
+        print('Black is playing.')
+        if game_type == 2 or game_type = 4:
+            bot_play(b)
+        else:
+            human_player(b)
+
+def bot_play(b: Board) -> None:
+    """Bot makes move and informs about move.
+    """
+    ne = next_move(b)
+    move(ne, b)
+    print(f"Bot has moved {_coordinates_move(ne)}")
+    Board_Composer(b)
+        
+def human_player(b: Board) -> None:
+    """asks human player to make move,
+       input is xy-xy
+       if incorrect input, calls itself again
+       if correct, effects a move on the board
+    """
+    print('Please make a move. Input for coordinates is format "a1".')
+    print('Please select a piece to move.')
+    source = input('')
+    print(f'You selected {source}, now choose where to move it')
+    target = _translatetoint(input(''))
+    source = _translatetoint(source)
+    move = (source, target)
+    if is_legal(move, b)
+        Move(move, b)
+        Board_Composer(b)
+    else:
+        print('Wrong input, please try again!')
+        human_player(b)
+
+def _coordinates_move(m: Move) -> str:
+    """Returns string describing board coordinates
+       for a specific move.
+    """
+    statement = f'{_translatetostr(m[0])}' + ' to ' + f'{_translatetostr(m[1])}'
+    return statement
+
+def _translatetostr(a: int) -> str:
+    ans = ''
+    ans = ans + [['a' if a % 5 == 1
+                 else 'b' if a % 5 == 2
+                 else 'c' if a % 5 == 3
+                 else 'd' if a % 5 == 4
+                 else 'e' if a % 5 == 0]
+                ['1' if a >= 21
+                 else '2' if 16 <= a <= 20
+                 else '3' if 11 <= a <= 15
+                 else '4' if 6 <= a <= 10
+                 else '5' if 1 <= a <= 5]]
+    return ans
+
+def _translatetoint(a: str) -> int:
+    x = (ord(a[0]) - 96)
+    y = (5 - a[1])
+    return (x + y * 5)
+    
+
+
+
 
 def _piece_draw(color: str) -> str:
     return "x" if color == "black" else "O" if color == "white" else "H"
